2872398d065bc0bbfa97fbc386742ee9111541a4
sumonelse <sumonelse01@gmail.com>
2024-09-02 21:26:07 +0530

    Update Server Config

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import app from "./src/app.js"
+import { config } from "./src/config/config.js"
+import connectDB from "./src/config/db.js"
 
-const startServer = () => {
-    const port = process.env.PORT || 3000
+const startServer = async () => {
+    await connectDB()
+
+    const port = config.port || 3000
 
     app.listen(port, () => {
         console.log(`Server started on PORT: ${port}`)
